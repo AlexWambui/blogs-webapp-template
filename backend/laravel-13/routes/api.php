7f2adcab99796acf26b48ebcdd64621456c6c858
alexwambui <alexwambuik@gmail.com>
2026-04-17 21:43:01 +0300

    installed fortify.

--- backend/laravel-13/routes/api.php
+++ backend/laravel-13/routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+Route::get('/users', function (Request $request) {
+    return User::all();
+});
