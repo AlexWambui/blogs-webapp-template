acc75a2ce5a08b6c28191692bc317d5df755e3a9
alexwambui <alexwambuik@gmail.com>
2026-04-17 21:43:27 +0300

    basic frontend structure.

---
+++ frontend/vuejs/src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/auth/Login.vue'
+import RegisterView from '../views/auth/Register.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+  ],
+})
+
+export default router
---
+++ frontend/vuejs/src/views/auth/Register.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import axios from 'axios';
+
+const register = async () => {
+    console.log('Registering...');
+
+    await axios.get('http://localhost:8000/sanctum/csrf-cookie', {
+        withCredentials: true,
+    });
+
+    try {
+        const response = await axios.post('http://localhost:8000/api/register', {
+            name: 'John Doe',
+            email: 'john@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+register();
+</script>
+
+<template>
+    <h1>Register</h1>
+</template>
