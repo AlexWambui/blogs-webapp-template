1fffee541bded4a9f7eff2a751528db86bfce171
alexwambui <alexwambuik@gmail.com>
2026-04-17 22:42:02 +0300

    updated the axios configurations.

--- frontend/vuejs/src/views/auth/Register.vue
+++ frontend/vuejs/src/views/auth/Register.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+axios.defaults.baseURL = "http://localhost:8000";
+
 const register = async () => {
     console.log('Registering...');
 
-    await axios.get('http://localhost:8000/sanctum/csrf-cookie', {
-        withCredentials: true,
-    });
+    await axios.get('/sanctum/csrf-cookie');
 
     try {
-        const response = await axios.post('http://localhost:8000/api/register', {
+        const response = await axios.post('/api/register', {
             name: 'John Doe',
-            email: 'john@gmail.com',
+            email: 'johndoe@gmail.com',
             password: 'password',
             password_confirmation: 'password',
         });
